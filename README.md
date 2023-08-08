efef1fdd4d3ce78b26adac3bcb1859238ea440eb
hareaka <ganger2012201@gmail.com>
2023-08-08 11:53:05 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Flutter clean architecture - Posts app 
+
+## using fake api from this website https://jsonplaceholder.typicode.com/
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'core/app_theme.dart';
+import 'features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
+import 'features/posts/presentation/bloc/posts/posts_bloc.dart';
+import 'features/posts/presentation/pages/posts_page.dart';
+import 'injection_container.dart' as di;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+              create: (_) => di.sl<PostsBloc>()..add(GetAllPostsEvent())),
+          BlocProvider(create: (_) => di.sl<AddDeleteUpdatePostBloc>()),
+        ],
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: appTheme,
+            title: 'Posts App',
+            home: PostsPage()));
+  }
+}
